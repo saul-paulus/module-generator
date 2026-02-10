53c60213c613b6cd078ac5993e716b2137399658
saul-paulus <saulpaulus17@icloud.com>
2026-02-10 23:49:03 +0700

    Fix: Memperbaiki semua file stub untuk api install

--- src/Providers/ModuleGeneratorServiceProvider.php
+++ src/Providers/ModuleGeneratorServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ixspx\ModuleGenerator\Providers;
 
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Ixspx\ModuleGenerator\Console\Commands\ApiInstallCommand;
 use Ixspx\ModuleGenerator\Console\Commands\ApiResponseMakeCommand;
@@ -22,6 +23,8 @@ final class ModuleGeneratorServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this->registerRoutes();
+
         if ($this->app->runningInConsole()) {
 
             $this->publishes([
@@ -35,5 +38,12 @@ final class ModuleGeneratorServiceProvider extends ServiceProvider
                 ApiInstallCommand::class,
             ]);
         }
+    }
+
+    protected function registerRoutes(): void
+    {
+        Route::prefix('api/v1')
+            ->middleware(['api', 'force.json'])
+            ->group(__DIR__ . '/../routes/api.php');
     }
 }
